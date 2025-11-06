7bc5a02061c7b28a32226ff6452f38f6d82497de
Jack Saenz <jack.p.saenz@gmail.com>
2025-11-06 17:36:10 -0600

    Testing Accel

--- main/i2c_basic_example_main.c
+++ main/i2c_basic_example_main.c
@@ -35,6 +35,14 @@ static const char *TAG = "example";
 #define LSM9DS1_I2C_ADDRESS 0x1E
 #define LSM9DS1_I2C_WHO_AM_I 0x0F
 
+#define LSM9DS1_CTRL_REG6_XL 0x20
+
+typedef struct {
+   int16_t x;
+   int16_t y;
+   int16_t z;
+} int16_vec3_t;
+
 static esp_err_t lsm9ds1_read_byte(i2c_master_dev_handle_t dev_handle,
                                    uint8_t reg_addr, uint8_t *data_buffer) {
    uint8_t write_buf[1] = {reg_addr};
@@ -50,6 +58,30 @@ static esp_err_t lsm9ds1_write_byte(i2c_master_dev_handle_t dev_handle,
                               I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
 }
 
+static int16_vec3_t lsm9ds1_get_accl_vec(i2c_master_dev_handle_t dev_handle) {
+   int16_vec3_t ret_vec;
+
+   uint8_t most_significant;
+   uint8_t least_significant;
+
+   ESP_ERROR_CHECK(lsm9ds1_read_byte(dev_handle, 0x28, &least_significant));
+   ESP_ERROR_CHECK(lsm9ds1_read_byte(dev_handle, 0x29, &most_significant));
+
+   ret_vec.x = (most_significant << 8) | least_significant;
+
+   ESP_ERROR_CHECK(lsm9ds1_read_byte(dev_handle, 0x2A, &least_significant));
+   ESP_ERROR_CHECK(lsm9ds1_read_byte(dev_handle, 0x2B, &most_significant));
+
+   ret_vec.y = (most_significant << 8) | least_significant;
+
+   ESP_ERROR_CHECK(lsm9ds1_read_byte(dev_handle, 0x2C, &least_significant));
+   ESP_ERROR_CHECK(lsm9ds1_read_byte(dev_handle, 0x2D, &most_significant));
+
+   ret_vec.z = (most_significant << 8) | least_significant;
+
+   return ret_vec;
+}
+
 /**
  * @brief i2c master initialization
  */
@@ -61,7 +93,7 @@ static void i2c_master_init(i2c_master_bus_handle_t *bus_handle,
        .scl_io_num = I2C_MASTER_SCL_IO,
        .clk_source = I2C_CLK_SRC_DEFAULT,
        .glitch_ignore_cnt = 7,
-       .flags.enable_internal_pullup = true,
+       .flags.enable_internal_pullup = false,
    };
    ESP_ERROR_CHECK(i2c_new_master_bus(&bus_config, bus_handle));
 
@@ -79,4 +111,14 @@ void app_main(void) {
    i2c_master_dev_handle_t dev_handle;
    i2c_master_init(&bus_handle, &dev_handle);
    ESP_LOGI(TAG, "I2C initialized successfully");
+
+   // Turn on accelerometer
+   ESP_ERROR_CHECK(
+       lsm9ds1_write_byte(dev_handle, LSM9DS1_CTRL_REG6_XL, 0b110 << 5));
+
+   while (true) {
+      int16_vec3_t vec = lsm9ds1_get_accl_vec(dev_handle);
+      printf("X:%6d Y:%6d Z:%6d\n", vec.x, vec.y, vec.z);
+      vTaskDelay(10 / portTICK_PERIOD_MS);
+   }
 }
